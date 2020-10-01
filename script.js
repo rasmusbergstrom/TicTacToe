d37f2263629425769b533c906b32f6be6e4f2237
Rasmus Bergström <Rasmusbergstrom@live.com>
2020-10-01 17:27:30 +0200

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var box;
+function myFunction(){
+    var y = document.getElementById("grid-container");
+    var x = document.getElementsByClassName("grid-gameblock");
+    var i; 
+    for(i = 0; i< x.length; i++) {
+        x[i].style.backgroundColor = "red";
+    }
+    
+  
+    
+    
+  
+    
+}
